6c799a2317bd2dfe1937b6e17e2714e6670b920b
Chey2502 <114569783+Chey2502@users.noreply.github.com>
2023-04-20 19:09:02 +0530

    Update ElectricityBillingSystem.java

--- ElectricityBillingSystem.java
+++ ElectricityBillingSystem.java
@@ -1,11 +1,16 @@
 package jay;
-import javax.swing.*;
-import java.awt.*;
 
-public class ElectricityBillingSystem {
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ElectricityBillingSystem3 {
+  private static JTextField customerNameText;
+  private static JTextField customerAddressText;
+  private static JTextField electricityUnitText;
+  
   public static void main(String[] args) {
     JFrame frame = new JFrame("Electricity Billing System");
-    frame.setSize(400, 200);
+    frame.setSize(800, 400);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
     JPanel panel = new JPanel();
@@ -13,7 +18,7 @@ public class ElectricityBillingSystem {
     placeComponents(panel);
     
     frame.setVisible(true);
-  }
+  	}
   
   private static void placeComponents(JPanel panel) {
     panel.setLayout(null);
@@ -22,7 +27,7 @@ public class ElectricityBillingSystem {
     customerNameLabel.setBounds(10, 10, 120, 25);
     panel.add(customerNameLabel);
     
-    JTextField customerNameText = new JTextField(20);
+    customerNameText = new JTextField(20);
     customerNameText.setBounds(140, 10, 165, 25);
     panel.add(customerNameText);
     
@@ -30,7 +35,7 @@ public class ElectricityBillingSystem {
     customerAddressLabel.setBounds(10, 40, 120, 25);
     panel.add(customerAddressLabel);
     
-    JTextField customerAddressText = new JTextField(20);
+    customerAddressText = new JTextField(20);
     customerAddressText.setBounds(140, 40, 165, 25);
     panel.add(customerAddressText);
     
@@ -38,12 +43,38 @@ public class ElectricityBillingSystem {
     electricityUnitLabel.setBounds(10, 70, 120, 25);
     panel.add(electricityUnitLabel);
     
-    JTextField electricityUnitText = new JTextField(20);
+    electricityUnitText = new JTextField(20);
     electricityUnitText.setBounds(140, 70, 165, 25);
     panel.add(electricityUnitText);
     
     JButton generateBillButton = new JButton("Generate Bill");
-    generateBillButton.setBounds(250, 100, 120, 25);
+    generateBillButton.setBounds(120, 100, 120, 25);
     panel.add(generateBillButton);
+    
+    generateBillButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        String customerName = customerNameText.getText();
+        String customerAddress = customerAddressText.getText();
+        int electricityUnit = Integer.parseInt(electricityUnitText.getText());
+        
+        int amount = electricityUnit * 10;
+        
+        JOptionPane.showMessageDialog(null, "Customer Name: " + customerName + "\nCustomer Address: " + customerAddress + "\nElectricity Unit: " + electricityUnit + "\nAmount: " + amount);
+      }
+    });
+    
+    JButton refreshButton = new JButton("Refresh");
+    refreshButton.setBounds(250, 100, 120, 25);
+    panel.add(refreshButton);
+    
+    refreshButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        customerNameText.setText("");
+        customerAddressText.setText("");
+        electricityUnitText.setText("");
+      }
+    });
   }
 }
+
+    
